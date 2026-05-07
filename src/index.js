7061ac36cc22b4735c78f53cefb6cbdaf26c65e9
krist0nian <krist0ian@yahoo.com>
2026-05-07 12:11:17 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags } from '@fortawesome/free-solid-svg-icons'
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags)
+
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
